427cd97e348fa45986a10941a8294862a98de870
Live session user <ian@brunelli.me>
2018-07-26 14:07:22 +0000

    Using getpass to ask for password.

--- boto_config_file.py
+++ boto_config_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 from appdirs import user_config_dir
+from configparser import ConfigParser
+from getpass import getpass
 from os import path
 import minervaboto
-import configparser
 import os
 import sys
 
@@ -12,11 +13,10 @@ def write_config_file(config, config_file_path):
         config.write(f)
 
 def input_login_info(config, config_file_path, operation):
-    config["LOGIN"]["MINERVA_ID"] = input('ID/CPF: ')
-    config["LOGIN"]["MINERVA_PASS"] = input('Senha (plain-text): ')
+    config['LOGIN']['MINERVA_ID'] = input('ID/CPF: ')
+    config['LOGIN']['MINERVA_PASS'] = getpass('Senha: ')
 
     write_config_file(config, config_file_path)
-
     print('Arquivo %s. Continuando...\n' % operation)
 
 def main():
@@ -26,8 +26,12 @@ def main():
     if not path.exists(boto_cfg_path):
         os.makedirs(boto_cfg_path)
 
-    config = configparser.ConfigParser(default_section="LOGIN")
+    config = ConfigParser(default_section='LOGIN')
     config.read(config_file_path)
+
+    login = config['LOGIN']
+    for required_key in ['MINERVA_ID', 'MINERVA_PASS']:
+        if not required_key in login: login[required_key] = ''
 
     if not path.exists(config_file_path):
         ans = input('Não encontramos um arquivo de configurações. Deseja ' +
@@ -35,25 +39,19 @@ def main():
         if not ans.lower().strip() in ['s', 'sim']:
             print('Tudo bem. Por favor, insira seu ID e senha em: \n\t[%s]' %
                   config_file_path)
-            config["LOGIN"]["MINERVA_ID"] = ''
-            config["LOGIN"]["MINERVA_PASS"] = ''
             write_config_file(config, config_file_path)
             sys.exit(0)
 
         input_login_info(config, config_file_path, 'salvo')
 
     while True:
-        try:
-            user_id = config["LOGIN"]["MINERVA_ID"]
-            user_password = config["LOGIN"]["MINERVA_PASS"]
-            if not user_id or not user_password: raise(KeyError)
-        except KeyError:
+        if not (login['MINERVA_ID'] and login['MINERVA_PASS']):
             print('Arquivo de configurações incompleto')
             input_login_info(config, config_file_path, 'atualizado')
             continue
 
         url = 'https://minerva.ufrj.br/F'
-        renewed = minervaboto.renew_books(user_id, user_password, url)
+        renewed = minervaboto.renew_books(login['MINERVA_ID'], login['MINERVA_PASS'], url)
 
         if renewed['result']:
             minervaboto.print_books(renewed['result'])
