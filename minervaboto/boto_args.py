6ccc2abcf65b34e72fa3102e6a304df67a266a09
Ian Brunelli <ian@brunelli.me>
2018-07-27 23:18:31 +0000

    Reducing the number of files.

--- minervaboto/boto_args.py
+++
@@ -1,8 +0,0 @@
-import minervaboto
-import sys
-
-def args():
-    url = 'https://minerva.ufrj.br/F'
-    renewed = minervaboto.renew_books(sys.argv[1], sys.argv[2], url)
-
-    print(minervaboto.renewed_to_string(renewed))
--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup
 setup(name='minervaboto',
       description='A renewal tool for Acervo Minerva',
       # long_description=long_description,
-      version='1.0.5',
+      version='1.0.6',
       url='https://github.com/erickpires/minervaboto',
       author='Erick Pires',
       author_email='rckkas@gmail.com',
       license='MIT',
       packages=['minervaboto'],
       install_requires=[
-          'beautifulsoup4',
-          'requests',
           'appdirs',
+          'beautifulsoup4',
           'configparser'
+          'requests',
       ],
       entry_points={
           'console_scripts': [
