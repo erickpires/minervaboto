21febd1c42d2ffc958134169e4ef26b66698f83b
Erick Pires <pires@erickpires.com>
2018-07-29 20:36:17 -0300

    Saving and loading files, plus key-bindings.

--- gui_app/minervaboto-gui.py
+++ gui_app/minervaboto-gui.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 from tkinter import *
-from tkinter.messagebox import showinfo
+from tkinter.messagebox import showinfo, showerror, showwarning
 from PIL import ImageTk, Image
+
+from minervaboto import utils
+
 import minervaboto
+import os
 
 default_font = ('verdana', 14)
 default_pad = 5
@@ -12,13 +16,14 @@ default_relief = FLAT
 
 class App:
     def __init__(self):
-        root = Tk()
-        root.title('Renovação Minerva')
-        root.resizable(False, False)
+        self.root = Tk()
+        self.root.title('Renovação Minerva')
+        self.root.resizable(False, False)
+        self.root.bind('<Escape>', self.close)
 
         self.save_var = IntVar()
 
-        main_frame = Frame(root)
+        main_frame = Frame(self.root)
         main_frame.pack(expand=True, fill=BOTH)
 
         logo_img = ImageTk.PhotoImage(Image.open('logo.png'))
@@ -46,7 +51,7 @@ class App:
         pass_label.pack(side=LEFT, padx=default_pad, pady=default_pad)
 
         self.pass_entry = Entry(second_row)
-        self.pass_entry.config(width=11, font=default_font)
+        self.pass_entry.config(width=11, font=default_font, show="*")
         self.pass_entry.pack(side=RIGHT, padx=default_pad)
 
 
@@ -64,14 +69,24 @@ class App:
         renew_button = Button(fourth_row, text='Renovar', command=self.renew_callback)
         renew_button.config(font=default_font)
         renew_button.pack(side=RIGHT, padx=default_pad, pady=default_pad)
+        renew_button.bind('<Return>', self.renew_callback)
+        renew_button.focus_set()
 
         self.fill_credentials()
 
-        root.mainloop()
+        self.root.mainloop()
 
-    def renew_callback(self):
+    def renew_callback(self, event=None):
         renewed = minervaboto.renew_books(self.id_entry.get(), self.pass_entry.get())
-        showinfo('Renovação', minervaboto.renewed_to_string(renewed))
+        if renewed['result']:
+            showinfo('Renovação', minervaboto.renewed_to_string(renewed))
+            self.save_credentials()
+        else:
+            if renewed['response']['code'] == 200:
+                showwarning('Renovação', minervaboto.renewed_to_string(renewed))
+                self.save_credentials()
+            else:
+                showerror('Renovação', minervaboto.renewed_to_string(renewed))
 
 
     def validate_id(self, input_str, action_type):
@@ -81,6 +96,27 @@ class App:
         return True
 
     def fill_credentials(self):
-        pass
+        config_file = utils.get_default_config_file('minervaboto', 'boto.conf')
+        if not os.path.exists(config_file): return
+
+        config = utils.read_config_file(config_file)
+        user_id, user_pass = utils.get_info_from_config(config)
+
+        if not (user_id and user_pass): return
+
+        self.id_entry.insert(0, user_id)
+        self.pass_entry.insert(0, user_pass)
+
+    def save_credentials(self):
+        if not self.save_var.get(): return
+        config_file = utils.get_default_config_file('minervaboto', 'boto.conf')
+        config = utils.read_config_file(config_file)
+
+        config['LOGIN']['MINERVA_ID'] = self.id_entry.get()
+        config['LOGIN']['MINERVA_PASS'] = self.pass_entry.get()
+        utils.write_config_file(config, config_file)
+
+    def close(self, event=None):
+        self.root.destroy()
 
 app = App()
